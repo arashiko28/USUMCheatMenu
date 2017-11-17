44317b31b5637d3971ca2cb8b96aa4a2af92d1a8
unipablojorge <pablogormi822@gmail.com>
2017-11-16 22:38:42 -0400

    USUM Changes
    Comment things I'm not using, max money cheat added

--- Sources/currency_modifiers.c
+++ Sources/currency_modifiers.c
@@ -14,13 +14,13 @@ static u32 quantity = 500000;
 // Currency menu entry
 void    currencyMenu(void) {
     new_spoiler("Currency");
-        new_entry_managed("Increase Quantity +500,000", increaseCurrencyQuantity, INCREASECURRENCYQUANTITY, AUTO_DISABLE);
+        //new_entry_managed("Increase Quantity +500,000", increaseCurrencyQuantity, INCREASECURRENCYQUANTITY, AUTO_DISABLE);
         new_separator();
         new_entry_managed("Poke Dollars          xCCCCCCC", maxMoney, MAXMONEY, EXECUTE_ONCE);
-        new_entry_managed("Battle Points         xCCCCCCC", maxBP, MAXBP, EXECUTE_ONCE);
-        new_entry_managed("Festival Coins        xCCCCCCC", maxCoins, MAXCOINS, EXECUTE_ONCE);
-        new_entry_managed("Total Festival Coins  xCCCCCCC", totalCoins, TOTALCOINS, EXECUTE_ONCE);
-        new_entry_managed("Thumbs Up             xCCCCCCC", totalThumbs, TOTALTHUMBS, EXECUTE_ONCE);
+        //new_entry_managed("Battle Points         xCCCCCCC", maxBP, MAXBP, EXECUTE_ONCE);
+        //new_entry_managed("Festival Coins        xCCCCCCC", maxCoins, MAXCOINS, EXECUTE_ONCE);
+        //new_entry_managed("Total Festival Coins  xCCCCCCC", totalCoins, TOTALCOINS, EXECUTE_ONCE);
+        //new_entry_managed("Thumbs Up             xCCCCCCC", totalThumbs, TOTALTHUMBS, EXECUTE_ONCE);
         new_line();
     exit_spoiler();
     updateCurrencyQuantity();
@@ -56,12 +56,16 @@ void    updateCurrencyQuantity(void) {
 
 // Set Poké Dollars
 void	maxMoney(void) {
-	WRITEU32(0x330D8FC0, quantity);
+	u32 offset = READU32(0x0066F3DC);
+	u32 offset2 = READU32(0x0000001C + offset);
+	u32 offset3 = READU32(0x00000024 + offset2);
+	u32 offset4 = READU32(0x00000004 + offset3);
+	WRITEU32(0x000038E8 + offset4, 0x0098967F);
 }
 
 // Set current Festival Coins to 999,999
 void	maxCoins(void) {
-    WRITEU32(0x33124D58, (quantity > 999999) ? 0x000F423F : quantity);
+
 }
 
 
@@ -84,4 +88,9 @@ void	totalThumbs(void) {
 // Set Battle Points
 void	maxBP(void) {
         WRITEU16(0x330D90D8, (quantity > 9999) ? 0x270F : quantity);
+}
+
+// Can learn any hm tm
+void 	anyMov(u32 state) {
+	WRITEU32(0x004AB044, (state) ? 0xE3A00001 : 0xE3A00000);
 }
